d10e8216497b6654ae9f68321ff5bfa513d44dbb
Tahir Jemal <jemalmtahir@gmail.com>
2020-03-14 18:16:05 +0200

    react jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+// const welcome = {
+//   greeting: "Hey",
+//   title: "React"
+// };
+function getTitle(title) {
+  return title;
+}
 function App() {
-  const title = "React";
+  //const title = "React";
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>Hello {getTitle("React")}</h1>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
     </div>
   );
 }
