f4384bb452a62bae520a9051b6bbd50e9c1c94db
Tahir Jemal <jemalmtahir@gmail.com>
2020-03-14 18:57:42 +0200

    handle function

--- src/App.js
+++ src/App.js
@@ -18,33 +18,33 @@ const list = [
     objectID: 1
   }
 ];
-function App() {
-  //const title = "React";
+const App = () => {
+  const handleChange = event => {
+    console.log("handleChange -> event", event.target.value);
+  };
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
 
       <hr />
       <List />
+      <List />
     </div>
   );
-}
+};
 
-function List() {
-  return list.map(function(item) {
-    return (
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </div>
-    );
-  });
-}
+const List = () =>
+  list.map(item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
 
 export default App;
